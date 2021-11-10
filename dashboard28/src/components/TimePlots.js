1f314d427a4b00b065e597fbf1359d2315ebf33a
T L <thiago.lopes0107@gmail.com>
2021-11-10 19:10:36 -0300

    first charts

--- dashboard28/src/components/TimePlots.js
+++ dashboard28/src/components/TimePlots.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './styles/Home.css';
 import img from './assets/img_ex.png'
-
+import data from './Data.js'
+import ChartEx from './ChartEx';
 export default function Time(props){
+    
+    console.log(data)
     return(
-        'time'
-    )
+        <div>
+        <ChartEx/>
+        <ChartEx/>
+        <ChartEx/>
+        </div>
+    );
 }
