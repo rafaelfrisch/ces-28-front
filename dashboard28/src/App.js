5059a4111c271d87a22f87990083ec9929f953c3
ArthurJose2000 <arthur2000jose@gmail.com>
2021-11-24 16:50:58 -0300

    Solve conflicts

--- dashboard28/src/App.js
+++ dashboard28/src/App.js
@@ -1,38 +1,25 @@
 import './App.css';
 import SideBar from './components/SideBar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
 import Products from './pages/Products';
 import Users from './pages/Users';
+import Home from './components/Home'
+import Dashboards from './components/Dashboards'
 
 function App() {
   return (
       <Router>
-        <SideBar />
+        <div className="App">
+          <SideBar />
           <Routes>
-            <Route path='/' element={<Dashboard />} />
             <Route path='/products' element={<Products />} />
             <Route path='/users' element={<Users />} />
-          </Routes>        
+            <Route path="/" element={<Home/>}/>
+            <Route path="/dashboards" element={<Dashboards/>}/>
+            <Route path="*" element={<h1>Rota não encontrada</h1>}/>
+          </Routes>
+          </div>        
       </Router> 
-import React from 'react';
-
-import Home from './components/Home'
-import Dashboards from './components/Dashboards'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        {/* <Nav/> */}
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/dashboards" element={<Dashboards/>}/>
-          <Route path="*" element={<h1>Rota não encontrada</h1>}/>
-        </Routes>
-      </div>
-    </Router>
   );
 }
 
