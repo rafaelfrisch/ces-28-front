adff2833898da19b74d5a85a63d9e7e378b5772a
ArthurJose2000 <arthur2000jose@gmail.com>
2021-11-24 15:59:18 -0300

    Products table

--- dashboard28/src/App.js
+++ dashboard28/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import SideBar from './components/SideBar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Products from './pages/Products';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <SideBar />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/users' element={<Users />} />
+          </Routes>        
+      </Router> 
   );
 }
 
