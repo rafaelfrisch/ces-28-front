02ac7ddb56291205de0e541878c3f6785d5efcc9
Erick Coelho <erickcoelho06@gmail.com>
2021-12-07 18:51:07 -0300

    make home page

---
+++ dashboard28/src/hook/places.js
@@ -0,0 +1,18 @@
+const places = [
+    {
+      title: 'Como funciona:',
+      description:
+        "O dono do negócio terá acesso a um dashboard com diversas visualizações de dados relativos a vendas e logística do negócio para que ele e seus funcionários possam visualizar os dados da operação e retirar insights.",
+      imageUrl: process.env.PUBLIC_URL + '/assets/img2.jpg',
+      time: 1500,
+    },
+    {
+      title: 'Quem Somos:',
+      description:
+        'Somos um grupo composto por 4 alunos do ITA com o propósito de ajudar donos de varejo a tomarem decisões mais inteligentes e assertivas baseadas em dados.',
+      imageUrl: process.env.PUBLIC_URL + '/assets/img1.jpg',
+      time: 1500,
+    },
+  ];
+  
+  export default places;
--- dashboard28/src/pages/Home.js
+++ dashboard28/src/pages/Home.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 import { useState } from 'react';
 import './styles/Home.css';
 import img from './assets/img_ex.png'
 import { Link } from 'react-router-dom';
 import Login from '../components/Login'
 import SideBar from '../components/SideBar';
+import Header from '../components/Header';
+import PlaceToVisit from '../components/PlaceToVisit';
 
 export default function Home(props){
 
     const [isLogged, setIsLogged] = useState(false);
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          minHeight: '100vh',
+          backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundColor: 'black',
+        },
+    }));
+    const classes = useStyles();
 
     if(localStorage.getItem("token") != null)
         return(
-            <div><SideBar/>
-                Home
+            <div className={classes.root}><SideBar/>
+                <CssBaseline />
+                <Header />
+                <PlaceToVisit />
+                Homee
             </div>
         )
     else
